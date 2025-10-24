f5a7572fea9773868c6268ff3630729cf8ee7222
Bpost129 <Bpost129@yahoo.com>
2025-10-24 12:58:24 -0400

    complete exercises 2 & 3

--- app.js
+++ app.js
@@ -23,7 +23,21 @@ Exercise 2:
   - Define a function as a function expression, `maxOfThree`, that takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
 */
 
-
+let maxOfThree = function(num1, num2, num3) {
+  if (num1 > num2) {
+    if (num1 > num3) {
+      return num1
+    } else {
+      return num3
+    }
+  } else {
+    if (num2 > num3) {
+      return num2
+    } else {
+      return num3
+    }
+  }
+}
 
 console.log("Exercise 2 Result:\n", maxOfThree(3, 9, 16))
 
@@ -32,7 +46,11 @@ Exercise 3:
   - Define a function, as a function declaration, `isCharAVowel` that takes a character as an argument and returns true if it is a vowel, false otherwise.
 */
 
+function isCharAVowel(char) {
+  const vowels = ['a', 'e', 'i', 'o', 'u']
 
+  return vowels.includes(char.toLowerCase())
+}
 
 console.log(
 `Exercise 3 Result:
